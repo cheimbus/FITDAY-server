2373f7ac3b168497d2e694c08d2bb88d71f027e3
Siu <sypark@SiuHwangui-MacBookPro.local>
2025-04-09 20:52:33 +0900

    feat: 인프라 세팅

--- scripts/start.sh
+++ scripts/start.sh
@@ -21,6 +21,12 @@ else
   echo "fitday-network 네트워크가 이미 존재합니다."
 fi
 docker-compose -f docker-compose.yml up -d
+if ! docker ps | grep -q nginxproxy; then
+  echo "nginxproxy 컨테이너가 실행 중이 아닙니다. docker-compose-nginx.yml 실행합니다."
+  docker-compose -f docker-compose-nginx.yml up -d
+else
+  echo "nginxproxy 컨테이너가 이미 실행 중입니다."
+fi
 docker pull 390402538983.dkr.ecr.ap-northeast-2.amazonaws.com/github-actions:latest
 docker run -d --name github-actions --network fitday-network -p 8080:8080 390402538983.dkr.ecr.ap-northeast-2.amazonaws.com/github-actions:latest
 echo "-------------End-------------"
