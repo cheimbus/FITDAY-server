e5bcfd2c3e7c0a951179d7e112ca356b3f394897
Siu <sypark@SiuHwangui-MacBookPro.local>
2025-04-10 01:24:55 +0900

    feat: 인프라 세팅

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,7 +8,6 @@ if [ ! -d "./certbot-etc" ]; then
 else
   echo "certbot-etc 폴더가 이미 존재합니다."
 fi
-cd docker
 docker stop github-actions || true
 docker stop fitDay-mysql || true
 docker stop fitDay-redis || true
