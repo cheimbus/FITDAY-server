d8edfa0e68405ebeea662e2cd308e7e078c10e2a
Siu <sypark@SiuHwangui-MacBookPro.local>
2025-04-09 20:24:03 +0900

    feat: 인프라 세팅

--- src/main/java/FITDAY/HelloController.java
+++ src/main/java/FITDAY/HelloController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api")
 @RestController
 @RequiredArgsConstructor
 public class HelloController {
     @GetMapping
     public String Hello() {
-        return "hi!!";
+        return "hi!!!";
     }
 }
