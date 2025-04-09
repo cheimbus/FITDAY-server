f73e8184095caf9b0c366448c738706180738181
Siu <sypark@SiuHwangui-MacBookPro.local>
2025-04-10 01:00:52 +0900

    feat: 인프라 세팅

--- src/main/java/FITDAY/HelloController.java
+++ src/main/java/FITDAY/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String Hello() {
-        return "서버세팅어렵다 gㅎㅎ";
+        return "서버세팅어렵다 gㅎㅎg";
     }
 }
