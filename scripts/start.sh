ae7c5b8db72e6060b325551f484698a762a61139
Siu <sypark@SiuHwangui-MacBookPro.local>
2025-04-09 16:45:10 +0900

    feat: 인프라 세팅

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,15 +8,25 @@ if [ ! -d "./certbot-etc" ]; then
 else
   echo "certbot-etc 폴더가 이미 존재합니다."
 fi
-docker network create fitday-network
 docker stop github-actions || true
 docker stop fitDay-mysql || true
 docker stop fitDay-redis || true
 docker rm github-actions || true
 docker rm fitDay-mysql || true
 docker rm fitDay-redis || true
-docker-compose up -d
+if ! docker network ls | grep -q fitday-network; then
+  docker network create fitday-network
+  echo "fitday-network 네트워크 생성 완료"
+else
+  echo "fitday-network 네트워크가 이미 존재합니다."
+fi
+if ! docker ps | grep -q nginxproxy; then
+  echo "nginxproxy 컨테이너가 실행 중이 아닙니다. docker-compose-nginx.yml 실행합니다."
+  docker-compose -f docker-compose-nginx.yml up -d
+else
+  echo "nginxproxy 컨테이너가 이미 실행 중입니다."
+fi
+docker-compose -f docker-compose.yml up -d
 docker pull 390402538983.dkr.ecr.ap-northeast-2.amazonaws.com/github-actions:latest
-docker run -d --name github-actions -p 8080:8080 390402538983.dkr.ecr.ap-northeast-2.amazonaws.com/github-actions:latest
-docker network create fitday-network
+docker run -d --name github-actions --network fitday-network -p 8080:8080 390402538983.dkr.ecr.ap-northeast-2.amazonaws.com/github-actions:latest
 echo "-------------End-------------"
