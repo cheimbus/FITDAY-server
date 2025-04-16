0940999e72d50aa23e6e69e2a4f7fb3bb1eb0ca6
Siu <siuh0403@gmail.com>
2025-04-16 13:56:53 +0900

    fix: db연동

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,11 +9,11 @@ else
   echo "certbot-etc 폴더가 이미 존재합니다."
 fi
 docker stop github-actions || true
-docker stop fitDay-mysql || true
-docker stop fitDay-redis || true
+#docker stop fitDay-mysql || true
+#docker stop fitDay-redis || true
 docker rm github-actions || true
-docker rm fitDay-mysql || true
-docker rm fitDay-redis || true
+#docker rm fitDay-mysql || true
+#docker rm fitDay-redis || true
 if ! docker network ls | grep -q fitday-network; then
   docker network create fitday-network
   echo "fitday-network 네트워크 생성 완료"
