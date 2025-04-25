b88464c30b72c5837e1c60ed48d34a97e44a561e
Siu <siuh0403@gmail.com>
2025-04-25 22:30:21 +0900

    feat: 파일 정리

--- src/main/java/FITDAY/weather/service/impl/WeatherServiceImpl.java
+++ src/main/java/FITDAY/weather/service/impl/WeatherServiceImpl.java
@@ -1,5 +1,6 @@
 package FITDAY.weather.service.impl;
 
+import FITDAY.common.service.LocationService;
 import FITDAY.global.Code;
 import FITDAY.global.WeatherCategory;
 import FITDAY.weather.dto.common.GetXY;
@@ -38,6 +39,7 @@ import java.util.*;
 public class WeatherServiceImpl implements WeatherService {
 
     private final RestTemplate restTemplate;
+    private final LocationService locationService;
     private static final List<String> TARGET_HOURS = List.of("0700", "1200", "2100");
 
     @Value("${weather.api-scheme}")
@@ -136,7 +138,7 @@ public class WeatherServiceImpl implements WeatherService {
 
     public List<WeatherHourData> setWeatherData(WeatherInfoRequest request) {
 
-        GetXY getXY = getXYByLocation(request.getPos());
+        GetXY getXY = locationService.getLocationInfo(request.getPos());
 
         // build true로 restTemplate 인코딩x 처리 => restTemplate은 자동 인코딩 기능때문에 2중으로 인코딩되어 공공 API에서 에러가 남.
         URI url = UriComponentsBuilder.newInstance()
@@ -195,29 +197,5 @@ public class WeatherServiceImpl implements WeatherService {
             }
         }
         return new ArrayList<>(dataMap.values());
-    }
-
-    public GetXY getXYByLocation(String location) {
-
-        return switch (location) {
-            case "서울특별시" -> new GetXY(60, 127);
-            case "부산광역시" -> new GetXY(98, 76);
-            case "대구광역시" -> new GetXY(89, 90);
-            case "인천광역시" -> new GetXY(55, 124);
-            case "광주광역시" -> new GetXY(58, 74);
-            case "대전광역시" -> new GetXY(67, 100);
-            case "울산광역시" -> new GetXY(102, 84);
-            case "세종특별자치시" -> new GetXY(66, 103);
-            case "경기도" -> new GetXY(60, 120);
-            case "강원도" -> new GetXY(73, 134);
-            case "충청북도" -> new GetXY(69, 107);
-            case "충청남도" -> new GetXY(68, 100);
-            case "전라북도" -> new GetXY(63, 89);
-            case "전라남도" -> new GetXY(51, 67);
-            case "경상북도" -> new GetXY(89, 91);
-            case "경상남도" -> new GetXY(91, 77);
-            case "제주특별자치도" -> new GetXY(52, 38);
-            default -> new GetXY(60, 127); // 서울로 디폴트 처리
-        };
     }
 }
